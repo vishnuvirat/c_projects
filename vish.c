d537751adb7e1674f4badbb86656f43bc3dd5080
Vishnu <vishnuviratkohli5@gmail.com>
2019-06-21 23:21:39 +0530

    vish.c file added

---
+++ vish.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	printf("\\     /\n \\   / \n  \\ /  \n   v   \n\t");
+	printf("-------\n\t   |   \n\t   |   \n\t   |   \n\t-------\n\t\t");
+	printf(" $$$$\n\t\t$    \n\t\t $$$ \n\t\t    $\n\t\t$$$$ \n\t\t\t");
+	printf("|   |\n\t\t\t|   |\n\t\t\t|---|\n\t\t\t|   |\n\t\t\t|   |\n\t\t\t\t");
+	printf("|   |\n\t\t\t\t|\\  |\n\t\t\t\t| \\ |\n\t\t\t\t|  \\|\n\t\t\t\t|   |\n\t\t\t\t\t");
+	printf("|   |\n\t\t\t\t\t|   |\n\t\t\t\t\t|   |\n\t\t\t\t\t|   |\n\t\t\t\t\t --- \n\t\t\t\t\t\t");
+}
+
+
+
